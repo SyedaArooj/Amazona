e3d6135fed50e35bdd0546c95fb1baed4f284c93
SyedaArooj <112927003+SyedaArooj@users.noreply.github.com>
2023-04-27 11:51:12 +0500

    amazona

---
+++ frontend/build/precache-manifest.d7211425ca75f9fa8df5336dcfe1cce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb84b36eb191cb32e56786ee828af78a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "390d269bef8f2ad7e35c",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390d269bef8f2ad7e35c",
+    "url": "/static/js/main.b4a8e246.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
